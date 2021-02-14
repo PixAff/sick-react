d313228bf243d8183fdf79dabc05b5a4b5168d1a
Stefan Sarow <stefan@pixelaffairs.com>
2021-02-14 21:20:06 +0100

    finished products page started sell

---
+++ frontend/components/CreateProduct.js
@@ -0,0 +1,109 @@
+import { useMutation } from "@apollo/client";
+import gql from "graphql-tag";
+import useForm from "../lib/useForm";
+import DisplayError from "./ErrorMessage";
+import { ALL_PRODUCTS_QUERY } from "./Products";
+import Form from "./styles/Form";
+import Router from "next/router";
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # which variables are getting passed in and what type are they?
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      name
+      description
+    }
+  }
+`;
+
+export default function CreateProduct() {
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: "",
+    name: "New Shoes",
+    price: 1900,
+    description: "These are great shoes!",
+  });
+
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
+    }
+  );
+  console.log(createProduct);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const res = await createProduct();
+    clearForm();
+    Router.push({
+      pathname: `/products/${res.data.createProduct.id}`,
+    });
+  }
+  return (
+    <Form onSubmit={handleSubmit}>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
+  );
+}
--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import gql from "graphql-tag";
 import Product from "./Product";
 
-const ALL_PRODUCTS_QUERY = gql`
+export const ALL_PRODUCTS_QUERY = gql`
   query ALL_PRODUCTS_QUERY {
     allProducts {
       id
@@ -28,14 +28,14 @@ const ProductList = styled.div`
 
 export default function Products() {
   const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
-  console.log(data, error, loading);
+  // console.log(data, error, loading);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
   return (
     <div>
       <ProductList>
         {data.allProducts.map((product) => (
-          <Product product={product} key={product.id}/>
+          <Product product={product} key={product.id} />
         ))}
       </ProductList>
     </div>
